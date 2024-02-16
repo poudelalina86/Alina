6bf1f305d27daa87412012aca0689704acf97cb6
Asmi Adhikari <asmiadhikari5-8@gmail.com>
2024-02-16 19:33:14 +0545

    new ui

--- home/models.py
+++ home/models.py
@@ -1,31 +1,31 @@
-from django.db import models
-from django.utils.text import slugify
-from django.contrib.auth import get_user_model
+# from django.db import models
+# from django.utils.text import slugify
+# from django.contrib.auth import get_user_model
 
-# Create your models here.
-User = get_user_model()
+# # Create your models here.
+# User = get_user_model()
 
-class Author(models.Model):
-    user = models.ForeignKey(User, on_delete= models.CASCADE)
-    fullname = models.CharField(max_length=40, blank=True)
-    slug = slug = models.SlugField(max_length=400,unique=True, blank=True)
+# class Author(models.Model):
+#     user = models.ForeignKey(User, on_delete= models.CASCADE)
+#     fullname = models.CharField(max_length=40, blank=True)
+#     slug = slug = models.SlugField(max_length=400,unique=True, blank=True)
 
-    def __str__(self):
-        return self.fullname
+#     def __str__(self):
+#         return self.fullname
 
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
-        super(Post,self).save(*args, **kwargs)
+#     def save(self, *args, **kwargs):
+#         if not self.slug:
+#             self.slug = slugify(self.title)
+#         super(Post,self).save(*args, **kwargs)
 
-class Post(models.Model):
-    title = models.CharField(max_length=400)
-    slug = models.SlugField(max_length=400, unique=True, blank=True)
-    user = models.ForeignKey(Author, on_delete = models.CASCADE)
-    content = HTMLField()
+# class Post(models.Model):
+#     title = models.CharField(max_length=400)
+#     slug = models.SlugField(max_length=400, unique=True, blank=True)
+#     user = models.ForeignKey(Author, on_delete = models.CASCADE)
+#     content = HTMLField()
     
 
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
-        super(Post,self).save(*args, **kwargs)
+#     def save(self, *args, **kwargs):
+#         if not self.slug:
+#             self.slug = slugify(self.title)
+#         super(Post,self).save(*args, **kwargs)
