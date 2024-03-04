19486798ad41f4ae4cd372fa047730d4c376f297
Asmi Adhikari <asmiadhikari5-8@gmail.com>
2024-03-05 00:15:37 +0545

    create added

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from home import views
 from users import views as user_views
-from .views import PostListView , PostDetailView
+from .views import PostListView , PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,6 +12,9 @@ urlpatterns = [
     path('loginpage',user_views.login_page,name="loginpage"),
     path('homepage', PostListView.as_view(),name='homepage'),
     path('homepage/post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('homepage/post/create/', PostCreateView.as_view(),name='post-create'),
+    path('homepage/post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+     path('homepage/post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('profile',user_views.profile,name="profile"),
     path('logout',views.logoutuser,name="logout"),
     
--- home/views.py
+++ home/views.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth import logout,login,authenticate
 from django.contrib.auth.decorators import login_required
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
 def index(request):
@@ -27,7 +28,44 @@ class PostListView(ListView):
 # home/post_list.html
 class PostDetailView(DetailView):
     model = Post
+    template_name= 'post_detail.html'
  
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name= 'post_form.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = 'homepage'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+   
+
 
 @login_required(login_url='/loginpage')
 def detailpage(request):
